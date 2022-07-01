350498badf31e5fd9b1c67c54b363bb1b2e55d8f
wpshamim <wpshamim@gmail.com>
2022-07-01 17:10:38 +0600

    Implement React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import "./App.css";
 
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Shared/Navbar/Navbar";
+import About from "./Components/Pages/About/About";
+import Appointments from "./Components/Pages/Appointments/Appointments";
+import Reviews from "./Components/Pages/Reviews/Reviews";
+import Profile from "./Components/Pages/Profile/Profile";
+import Contact from "./Components/Pages/Contact/Contact";
+import Home from "./Components/HomePage/Home/Home";
+import Login from "./Components/Pages/Login/Login";
+
 function App() {
     return (
-        <div>
-            <h1 className="text-error text-2xl">hellow</h1>
-        </div>
+        <>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/appointments" element={<Appointments />} />
+                <Route path="/reviews" element={<Reviews />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/login" element={<Login />} />
+
+                <Route path="/profile" element={<Profile />} />
+
+                <Route path="*" element />
+            </Routes>
+        </>
     );
 }
 
