acbe10eff35f0e41518903c010bb2a82f416dd9c
wpshamim <wpshamim@gmail.com>
2022-07-01 17:11:32 +0600

    Customize tailwind theme with my own theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,28 +2,33 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#19D3AE",
+                secondary: "#0FCFEC",
+                darker: "#3A4256",
+            },
+        },
+        container: {
+            padding: {
+                DEFAULT: "1rem",
+                sm: "2rem",
+                lg: "4rem",
+                xl: "5rem",
+                "2xl": "5rem",
+            },
+        },
     },
     daisyui: {
         themes: [
             {
-                mytheme: {
-                    primary: "#19D3AE",
-
-                    secondary: "#0FCFEC",
-
+                myTheme: {
                     accent: "#37CDBE",
-
                     neutral: "#3A4256",
-
                     "base-100": "#FFFFFF",
-
                     info: "#3ABFF8",
-
                     success: "#36D399",
-
                     warning: "#FBBD23",
-
                     error: "#F87272",
                 },
             },
