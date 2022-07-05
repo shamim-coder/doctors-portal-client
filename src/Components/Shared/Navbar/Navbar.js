1bfad8b923e4a3220b4caa60bbb6573697655fc8
wpshamim <wpshamim@gmail.com>
2022-07-05 21:41:03 +0600

    create dynamic navbar, take data from server

--- src/Components/Shared/Navbar/Navbar.js
+++ src/Components/Shared/Navbar/Navbar.js
@@ -1,5 +1,8 @@
-import React from "react";
+import { signOut } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link, NavLink } from "react-router-dom";
+import useNavItems from "../../../Hooks/useNavItems";
+import auth from "../../Utilities/Firebase.init";
 
 const Navbar = () => {
     const activeStyle = {
@@ -8,90 +11,77 @@ const Navbar = () => {
         borderRadius: "5px",
     };
 
-    const navItems = [
-        {
-            name: "Home",
-            path: "/",
-        },
-        {
-            name: "About",
-            path: "/about",
-        },
-        {
-            name: "Appointments",
-            path: "/appointments",
-        },
-        {
-            name: "Reviews",
-            path: "/reviews",
-        },
-        {
-            name: "Contact Us",
-            path: "/contact",
-        },
-        {
-            name: "Login",
-            path: "/login",
-        },
-    ];
+    const { navItems } = useNavItems();
+    const [user] = useAuthState(auth);
 
     return (
         <header className="py-5 shadow">
-            <div class="navbar bg-base-100 container mx-auto md:px-20">
-                <div class="navbar-start flex-1">
-                    <d iv class="dropdown">
-                        <label tabindex="0" class="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
+            <div className="navbar bg-base-100 container mx-auto md:px-20">
+                <div className="navbar-start flex-1">
+                    <div className="dropdown">
+                        <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
                             </svg>
                         </label>
-                        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                             {navItems.map((nav) => (
-                                <li>
+                                <li key={nav._id}>
                                     <Link to={nav.path}>{nav.name}</Link>
                                 </li>
                             ))}
                         </ul>
-                    </d>
-                    <Link to="/" class="font-semibold text-2xl">
+                    </div>
+                    <Link to="/" className="font-semibold text-2xl">
                         Doctors Portal
                     </Link>
                 </div>
 
-                <div class="flex-none gap-2 ">
-                    <div class="hidden lg:flex">
-                        <ul class="flex gap-5 p-0 mr-5">
+                <div className="flex-none gap-2 ">
+                    <div className="hidden lg:flex">
+                        <ul className="flex gap-5 p-0 mr-5">
                             {navItems.map((nav) => (
-                                <li>
+                                <li key={nav._id}>
                                     <NavLink style={({ isActive }) => (isActive ? activeStyle : undefined)} className="px-3 py-2" to={nav.path}>
                                         {nav.name}
                                     </NavLink>
                                 </li>
                             ))}
-                        </ul>
-                    </div>
 
-                    <div class="dropdown dropdown-end ">
-                        <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-                            <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                <img className="" src="https://placeimg.com/220/220/people" alt="" />
-                            </div>
-                        </label>
-                        <ul tabindex="0" class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-                            <li>
-                                <NavLink to="/profile" class="justify-between">
-                                    Profile
-                                    <span class="badge">New</span>
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/settings">Settings</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/login">Logout</NavLink>
-                            </li>
+                            {!user && (
+                                <li>
+                                    <NavLink style={({ isActive }) => (isActive ? activeStyle : undefined)} className="px-3 py-2" to="/login">
+                                        Login
+                                    </NavLink>
+                                </li>
+                            )}
                         </ul>
                     </div>
+                    {user && (
+                        <div className="dropdown dropdown-end ">
+                            <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
+                                <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                                    <img className="" src={user && auth?.currentUser?.photoURL} alt="" />
+                                </div>
+                            </label>
+                            <ul tabIndex="0" className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                                <li>
+                                    <NavLink to="/profile" className="justify-between">
+                                        Profile
+                                        <span className="badge">{user?.displayName}</span>
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/settings">Settings</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink onClick={() => signOut(auth)} to="/login">
+                                        Logout
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                    )}
                 </div>
             </div>
         </header>
